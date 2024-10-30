01eb242d3c382903dbdf3f759e5ee5584b7a72ce
unknown <adrianapaivausa@icloud.com>
2024-10-30 15:59:34 -0400

    color transition added

--- src/App.tsx
+++ src/App.tsx
@@ -9,45 +9,69 @@ type Quote = {
 };
 
 const getRandomQuote = (): Quote => {
-    return quotes[Math.floor(Math.random() * quotes.length)];
-  };
+  return quotes[Math.floor(Math.random() * quotes.length)];
+};
 
+const getRandomColor = (): string => {
+  const red = Math.floor(Math.random() * 128);
+  const green = Math.floor(Math.random() * 128);
+  const blue = Math.floor(Math.random() * 128);
+
+  return `rgb(${red}, ${green}, ${blue})`;
+};
+
+const transition = "all 1s";
 
 function App() {
   const [quote, setQuote] = useState<Quote>(getRandomQuote());
+  const [randomColor, setRandomColor] = useState<string>(getRandomColor());
 
   const changeQuote = () => {
     setQuote(getRandomQuote());
-  }
+    setRandomColor(getRandomColor());
+  };
 
   return (
-    <div className="background">
-    <div id="quote-box">
-        <div className="quote-content">
-            <FaQuoteLeft size="30" style={{ marginRight: "10px" }} />
-            <h2 id="text">{quote.quote}</h2>
-            <FaQuoteRight size="30" style={{ marginLeft: "10px" }} />
-            <h4 id="author">- {quote.author}</h4>
+    <div
+      className="background"
+      style={{
+        backgroundColor: randomColor,
+        transition
+      }}>
+      <div id="quote-box">
+        <div className="quote-content" style={{
+            color: randomColor,
+            transition
+        }}>
+          <h2 id="text">
+          <FaQuoteLeft size="30" style={{ marginRight: "10px" }} />
+            {quote.quote}
+            <FaQuoteRight size="30" style={{ marginLeft: "10px" }} />            
+            </h2>
+          <h4 id="author">- {quote.author}</h4>
         </div>
         <div className="buttons">
-            <a 
+          <a
             href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${quote.quote}`}
             id="tweet-quote"
             style={{
-                backgroundColor: "#1DA1F2",
-                marginRight: "10px",
+              backgroundColor: randomColor,
+              marginRight: "10px",
+              transition
             }}
-            >
-                <FaTwitter color="white"/>
-            </a>
-            <button id="new-quote" onClick={changeQuote}>
-                Change Quote
-            </button>
+          >
+            <FaTwitter color="white" />
+          </a>
+          <button id="new-quote" onClick={changeQuote} style={{
+            backgroundColor: randomColor, 
+            transition
+          }}>
+            Change Quote
+          </button>
         </div>
+      </div>
     </div>
-  </div>
   );
-};
-
+}
 
 export default App;
